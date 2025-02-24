bb21fbb3ea9857fccf9299da571cc72f0645bf94
PitreC1 <carlospitre@unicauca.edu.co>
2025-02-24 15:11:36 -0500

    crea funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let amigos = []; //Array para guardar el nombre de los amigos
 
+
+
+function agregarAmigo(){
+    if (document.getElementById("amigo").value == ''){
+        //Cuando este vacio muestre una alerta
+        alert("Por favor, inserte un nombre")
+    }
+    else{
+        //cuando ingrese un nombre guarde en lista
+        amigos.push(document.getElementById("amigo").value);
+        document.getElementById("amigo").value = "";
+    }   
+}
+
+function actualizarListaAmigos(){
+    
+}
